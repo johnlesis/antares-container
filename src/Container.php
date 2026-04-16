a12575359521a0585a3abb4b2d4ee3c87439861c
Johnlesis <johnlesis91@gmail.com>
2026-04-16 15:01:25 +0300

    feat: add circular dependency detection

--- src/Container.php
+++ src/Container.php
@@ -9,6 +9,7 @@ final class Container
     private array $bindings = [];
     private array $instances = [];
     private array $singletons = [];
+    private array $building = [];
 
     public function bind(string $interface, string $className): void
     {
@@ -34,10 +35,18 @@ final class Container
         if(isset($this->bindings[$className])) {
             return $this->make($this->bindings[$className]);
         } else {
+
+            if (isset($this->building[$className])) {
+                throw new ContainerException("Circular dependency detected: {$className}");
+            }
+            
+            $this->building[$className] = true;
+            
             $reflectionClass = new ReflectionClass($className);
 
             $constructor = $reflectionClass->getConstructor();
             if ($constructor === null) {
+                unset($this->building[$className]);
                 return new $className;
             }
 
@@ -46,13 +55,13 @@ final class Container
 
             foreach ($parameters as $parameter) {
                 $type = $parameter->getType();
-
                 if ($type instanceof \ReflectionNamedType && !$type->isBuiltin()) {
                     $args[] = $this->make($type->getName());
                 } elseif ($parameter->isDefaultValueAvailable()) {
                     $args[] = $parameter->getDefaultValue();
                 }
             }
+            unset($this->building[$className]);
             return $reflectionClass->newInstanceArgs($args);
         }
     }
--- test.php
+++ test.php
@@ -35,3 +35,18 @@ class FileLogger implements LoggerInterface {}
 $container->bind(LoggerInterface::class, FileLogger::class);
 $logger = $container->make(LoggerInterface::class);
 var_dump($logger instanceof FileLogger); // should be true
+
+
+class C {
+    public function __construct(D $d) {}
+}
+
+class D {
+    public function __construct(C $c) {}
+}
+
+try {
+    $container->make(C::class);
+} catch (ContainerException $e) {
+    echo $e->getMessage() . "\n";
+}
